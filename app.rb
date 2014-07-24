4962b07ac76e68856aa23d8b60b5cecca447076d
jyoji tada <george.6809@gmail.com>
2014-07-24 17:47:13 +0900

    Herokuで動かなかったので、動くかどうか確認する

--- app.rb
+++ app.rb
@@ -2,10 +2,5 @@ require 'sinatra'
 require 'json'
 
 get '/' do
-  request = Rack::Request.new(env)
-  json = JSON.generate({
-      ip: request.ip,
-      agent: request.user_agent
-    })
-  "#{json}"
+  "Hello world!"
 end
