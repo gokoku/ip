ce5387b5e83e302e73d86abc6c158f26be5b9b0c
jyoji tada <george.6809@gmail.com>
2014-09-10 15:01:39 +0900

    JSONに頭文字を付けた.

--- app.rb
+++ app.rb
@@ -3,8 +3,7 @@ require 'json'
 
 get '/' do
   request = Rack::Request.new(env)
-  JSON.generate([{
-      ip: request.ip,
-      agent: request.user_agent
-    }])
+  JSON.generate( { access:
+                  [{ ip: request.ip,
+                     agent: request.user_agent }]} )
 end
